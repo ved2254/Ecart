91623b2f389bcb88ed0df5afc08cd2bf5f822bbe
Ved prajapati <vedprajapati.vp1@gmail.com>
2026-03-11 14:01:34 +0530

    Fixed case sensitivity issue in ProtectedRoute import

--- backend/database/db.js
+++ backend/database/db.js
@@ -4,6 +4,8 @@ const connectDB = async() =>{
     try {
         await mongoose.connect(process.env.MONGO_URI)
         console.log('MongoDB connected sucessfully   ');
+            console.log("Database Name:", mongoose.connection.name);
+
         
     } catch (error) {
         console.log("MongoDB connection failed",error);
